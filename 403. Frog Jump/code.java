4e57ea5aeb5648760754a536d221ebb8fe244f93
CkyZzzz <cky.learner@gmail.com>
2018-08-09 13:32:07 -0700

    Aug 9 Second Commit

--- 403. Frog Jump/code.java
+++ 403. Frog Jump/code.java
@@ -6,7 +6,7 @@ class Solution {
 		for(int i = 0; i < stones.length - 1; i++){
 			Set<Integer> units = map.get(i);
 			for(int j = i + 1; j < stones.length; j++){
-				//这里如果units里不存在stones[j] - stones[i]的话不能马上break掉，当前position能否到达和后面的position能否到达没有关系
+				//这里如果units里不存在值=(stones[j] - stones[i])的话不能马上break掉，因为当前position能否到达和后面的position能否到达没有关系
 				if(units.contains(stones[j] - stones[i])){
 					int currUnits = stones[j] - stones[i];
 					map.get(j).add(currUnits - 1);
