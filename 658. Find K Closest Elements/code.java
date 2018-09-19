c8aa247a0c42962dfedb26c642504c9c2bb5b505
CkyZzzz <cky.learner@gmail.com>
2018-09-19 11:05:08 -0700

    Sep 19

--- 658. Find K Closest Elements/code.java
+++ 658. Find K Closest Elements/code.java
@@ -1,43 +1,53 @@
 class Solution {
-	public List<Integer> findClosestElements(int[] nums, int k, int x) {
-		List<Integer> result = new ArrayList<>();
-		if(x <= nums[0]){
-			for(int i = 0; i < k; i++) result.add(nums[i]);
-		}else if(x >= nums[nums.length - 1]){
-			for(int i = nums.length - 1; i > nums.length - 1 - k; i--) result.add(nums[i]);
-		}else{
-			int left = -1, right = -1;
-			for(int i = 0; i < nums.length; i++){
-				if(nums[i] > x){
-					right = i;
-					left = i - 1;
-					break;
-				}
-			}
-			int i = left, j = right;
-			while(i >= 0 && j < nums.length){
-				if(x - nums[i] <= nums[j] - x){
-					result.add(nums[i]);
-					i--;
-				}else{
-					result.add(nums[j]);
-					j++;
-				}
-				k--;
-				if(k == 0) break;
-			}
-			while(i >= 0 && k > 0){
-				result.add(nums[i]);
-				i--;
+	//T = O(Max(k, logn))
+	public List<Integer> findClosestElements(int[] arr, int k, int x) {
+		LinkedList<Integer> result = new LinkedList<>();
+		int index = 0;
+		if(x <= arr[0]){
+			while(k > 0){
+				result.add(arr[index++]);
 				k--;
 			}
-			while(j < nums.length && k > 0){
-				result.add(nums[j]);
-				j++;
+			return result;
+		}
+		index = arr.length - k;
+		if(x >= arr[arr.length - 1]){
+			while(k > 0){
+				result.add(arr[index++]);
+				k--;
+			}
+			return result;
+		}
+		int start = 0, end = arr.length - 1;
+		while(start + 1 < end){
+			int mid = start + (end - start) / 2;
+			if(arr[mid] < x){
+				start = mid;
+			}else{
+				end = mid;
+			}
+		}
+		while(start >= 0 && end < arr.length && k > 0){
+			if(x - arr[start] <= arr[end] - x){
+				result.addFirst(arr[start]);
+				start--;
+				k--;
+			}else{
+				result.addLast(arr[end]);
+				end++;
 				k--;
 			}
 		}
-		Collections.sort(result);
+		while(start >= 0 && k > 0){
+			result.addFirst(arr[start]);
+			start--;
+			k--;
+		}
+		while(end < arr.length && k > 0){
+			result.addLast(arr[end]);
+			end++;
+			k--;
+		}
 		return result;
 	}
 }
